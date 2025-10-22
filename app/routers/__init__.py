7e7cd6acf7c2e87d9ad6c942d67eea39404bcfdb
xamidovscode <xamidovcoder@gmail.com>
2025-10-22 22:00:08 +0500

    some big changes

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -4,8 +4,7 @@ from .start import build_start_router
 def build_root_router() -> Router:
     root = Router(name="root")
 
-    CHANNEL_ID = 1
-    CHANNEL_LINK = 'https://t.me/imb_edu'
+    channel_link = 'https://t.me/imb_edu'
 
-    root.include_router(build_start_router(channel=CHANNEL_ID, channel_link=CHANNEL_LINK))
+    root.include_router(build_start_router(channel=channel_link, channel_link=channel_link))
     return root
